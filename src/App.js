cbccd636a584840da3f5792e8852dcea77b2bc97
Yang <hao.frank@gmail.com>
2021-09-26 15:01:55 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,15 @@
-import logo from './logo.svg';
 import './App.css';
+// from Recipts_Book
+import Menu from './Recipts_Book/recipes';
+import { data } from './Recipts_Book/recipes';
+// from 
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className='App'>
+            <Menu recipes={data} title='Delicious Recipes' />
+        </div>
+    );
 }
 
 export default App;
